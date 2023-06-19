a41cad49f3fcda92e3631b7c6af30806c5e9bd6f
ascertain <kas.naya@gmail.com>
2023-06-19 10:55:13 -0700

    dynamic update

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -26,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* MAIN_H */
